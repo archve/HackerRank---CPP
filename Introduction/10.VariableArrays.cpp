8d18626b6c0cd8878958785aa3f1e0a5a579f437
archve <archve@gmail.com>
2018-08-25 20:35:02 +0530

    Create 10.VariableArrays.cpp

---
+++ Introduction/10.VariableArrays.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n,q; // number of arrays and queries
+    cin>>n>>q;
+    vector<int> arr[n];
+    for(int i=0;i<n;i++){
+        int arrsize = 0;
+        cin>>arrsize;
+        for(int j =0;j<arrsize;j++){
+            int temp;
+            cin>>temp;
+            arr[i].push_back(temp);
+        }
+    }
+    //responding to queries
+    for(int i_q=0;i_q<q;i_q++){
+        int i,j;
+        cin>>i>>j;
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        cout<<arr[i].at(j)<<endl;
+    }
